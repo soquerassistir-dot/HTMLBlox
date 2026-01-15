f05441dbc12b1bd7c42ce05e082e041891fc6a20
soquerassistir-dot <soquerassistir@gmail.com>
2026-01-15 17:56:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
 
-const ADMIN_KEY = "1234"; // 🔐 MUDA ISSO
+const ADMIN_KEY = "68@nisRY"; // 🔐 MUDA ISSO
 
 const app = express();
 const server = http.createServer(app);
@@ -113,3 +113,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("🚀 Online na porta", PORT);
 });
+
